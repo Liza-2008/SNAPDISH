4dcecf4b800f0e9d7c01a01ff004bef59c78595f
Liza-2008 <liza0525.becse24@chitkara.edu.in>
2025-06-24 23:42:24 +0530

    did some changes in the code

--- add-recipe.js
+++ add-recipe.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
     };
 
     try {
-      const res = await fetch('http://localhost:5000/api/recipes/add', {
+      const res = await fetch('http://127.0.0.1:5000/api/recipes/add', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data),
